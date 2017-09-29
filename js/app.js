e0ae944d7be36049b93426d60c70c6acec20e266
Manan12 <manan@mokko.io>
2017-09-29 22:26:49 +0530

    Folder Structure, Added the Calender Icon,  Final Commit

--- js/app.js
+++ js/app.js
@@ -1,20 +1,15 @@
 var app = angular.module('app', [ '720kb.datepicker']);
 
 app.controller('mainCtrl', function($scope, $http) {
-
     
-    
-    // var from = '2017-09-10';
-    // var to = '2017-09-13';
     $scope.from = "";
     $scope.to = "";
     $scope.currentDate = (new Date()).toString();
     $scope.data;
-       
-    $scope.clearSearch = function () {
-      $scope.from = "";
-      $scope.to = "";
-  };
+//     $scope.clearSearch = function () {
+//       $scope.from = "";
+//       $scope.to = "";
+//   };
 
     $scope.search = function(){
       $http.get(`http://104.197.128.152/data/adrequests?from=${$scope.from}&to=${$scope.to}`)
