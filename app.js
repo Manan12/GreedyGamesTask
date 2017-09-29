3c1e936598267033bc301d0a981a04d29af9689d
Manan12 <manan@mokko.io>
2017-09-29 11:55:33 +0530

    From date and To date functionality Added.

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ var app = angular.module('app', [ '720kb.datepicker']);
 
 app.controller('mainCtrl', function($scope, $http) {
 
-    $scope.currentDate = new Date();
+    
+    
+    // var from = '2017-09-10';
+    // var to = '2017-09-13';
+    $scope.from = "";
+    $scope.to = "";
+    $scope.currentDate = (new Date()).toString();
     console.log($scope.currentDate);
-    var from = '2017-09-10';
-    var to = '2017-09-13';
-       $http.get(`http://104.197.128.152/data/adrequests?from=${from}&to=${to}`)
+    console.log(new Date());
+    $scope.data;
+       
+
+    $scope.search = function(){
+      $http.get(`http://104.197.128.152/data/adrequests?from=${$scope.from}&to=${$scope.to}`)
          .then(function(res){
             $scope.data = res.data;
             console.log($scope.data);
           });
+    }
         
   });
